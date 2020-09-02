e4850c60c1087a8cb8d74052e52e0d0be16222bf
shivneel <wxx6957@autuni.ac.nz>
2020-09-02 22:54:53 +1200

    This is a test for the server side to work. Previous builds would compile, but would not show up anything on the screen / error out. This build should stop that.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Deploying a server.
+//https://medium.com/better-programming/how-to-deploy-your-react-app-to-heroku-aedc28b218ae
+//remote access not working?
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World~
+          Hello World~ This is a test on whether I can get the heroku server running properly again.
         </p>
         <a
           className="App-link"
