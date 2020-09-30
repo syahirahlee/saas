66a9ade03b8f65967ee8c01bcfe2c43ab141d0f7
Zhouqichang <whzqc@live.com>
2020-09-30 22:22:23 +1300

    wip

--- server/server.js
+++ server/server.js
@@ -4,9 +4,18 @@ const app = express();
 //Following is backed up just in case it doesnt work.
 //const publicPath = path.join(__dirname, '..', 'public');
 const publicPath = path.join(__dirname, '..', 'build');
-const port = process.env.PORT || 3000;app.use(express.static(publicPath));app.get('*', (req, res) => {
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
-});app.listen(port, () => {
+});
+app.get('/aa', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => {
     console.log('Server is up!');
     console.log('call localhost:3000 to run');
 });
