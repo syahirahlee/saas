7a490bf7c8809d0b1070f3b3ffe627343c50a8af
syahirahlee <62451840+syahirahlee@users.noreply.github.com>
2020-09-30 23:21:21 +1300

    Update formfield input text
    -change the input textfield to black as background is white

--- src/pages/SignInForm.js
+++ src/pages/SignInForm.js
@@ -46,7 +46,7 @@ class SignInForm extends Component {
               </div>
 
               <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign In</button> <Link to="/sign-up" className="FormField__Link">Create an account</Link>
+                <button className="FormField__Button mr-20">Sign In</button> <Link to="/sign-up" className="FormField__Link">Create an account</Link>
               </div>
             </form>
           </div>
